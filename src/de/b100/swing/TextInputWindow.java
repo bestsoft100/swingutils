308f7ae668789c57bbcf0c0e60d2b603cb253c2f
bestsoft100 <bestsoft100@gmail.com>
2020-03-20 22:08:48 +0100

    Style Change

--- src/de/b100/swing/TextInputWindow.java
+++ src/de/b100/swing/TextInputWindow.java
@@ -1,8 +1,11 @@
 package de.b100.swing;
 
+import java.awt.Color;
+import java.awt.Insets;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
+import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -13,6 +16,8 @@ public abstract class TextInputWindow implements ActionListener{
 	
 	private ArrayList<Input> textFields;
 	private ArrayList<JButton> buttons;
+	
+	private String title;
 	
 	private JFrame frame;
 	private JGridPanel panel;
@@ -33,16 +38,25 @@ public abstract class TextInputWindow implements ActionListener{
 		textFields.add(new Input(name, textField));
 	}
 	
+	protected void setTitle(String title) {
+		this.title = title;
+	}
+	
 	public void create() {
 		if(frame != null) {
 			close();
 		}
 		frame = new JFrame();
 		panel = new JGridPanel(10);
-		textFieldPanel = new JGridPanel(4);
+		textFieldPanel = new JGridPanel(6);
 		buttonPanel = new JPanel();
 		
 		textFieldPanel.setFillMode(2);
+		textFieldPanel.setBackground(Color.white);
+		textFieldPanel.setBorder(BorderFactory.createLineBorder(Color.LIGHT_GRAY));
+		
+		panel.getGridBagConstraints().ipadx = 100;
+		
 		int i=0;
 		while(i < textFields.size()) {
 			textFieldPanel.add(new JLabel(textFields.get(i).name), 0, i, 1, 1, 0, 1);
@@ -56,13 +70,17 @@ public abstract class TextInputWindow implements ActionListener{
 			j++;
 		}
 
+		panel.getGridBagConstraints().insets = new Insets(10, 0, 10, 0);
+		if(title != null) {
+			panel.add(new JLabel(title), 0, 0);
+		}
 		panel.add(textFieldPanel, 0, 0, 1, 1, 1, 1);
 		panel.add(buttonPanel, 0, 1, 1, 1, 1, 1);
 		
 		frame.add(panel);
 		frame.pack();
 		frame.setLocationRelativeTo(null);
-		frame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setVisible(true);
 	}
 	
